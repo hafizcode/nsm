4a944a11a2cfd6432adca4f3d3eb735e75b24d50
hafizcode <82789669+hafizcode@users.noreply.github.com>
2025-04-26 00:54:32 +0530

    added updated project

---
+++ core/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import UploadedFile, Category, Profile, SubjectGroup
+
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model = SubjectGroup
+        fields = ['name']
+
+
+class UploadFileForm(forms.ModelForm):
+    class Meta:
+        model = UploadedFile
+        fields = ['title', 'label', 'file', 'category', 'group']
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+class StaffCreationForm(UserCreationForm):
+    full_name = forms.CharField()
+    phone = forms.CharField()
+    department = forms.CharField()
+    email = forms.EmailField()
+    profile_image = forms.ImageField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class EditStaffForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['full_name', 'phone', 'department', 'profile_image']
---
+++ core/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class SubjectGroup(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class UploadedFile(models.Model):
+    title = models.CharField(max_length=255)
+    label = models.CharField(max_length=255)
+    file = models.FileField(upload_to='uploads/')
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    group = models.ForeignKey(SubjectGroup, on_delete=models.CASCADE, related_name='documents', default=1)
+
+
+    def __str__(self):
+        return self.title
+
+def default_profile_image():
+    return 'profile_pics/default.png'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15, blank=True)
+    department = models.CharField(max_length=100)
+    profile_image = models.ImageField(upload_to='profile_pics/', default=default_profile_image)
+
+    def __str__(self):
+        return self.user.username
